bce41b28237c24b21f1187d9feeb9bfb79ca9716
hardik <hardik32904@gmail.com>
2023-06-19 11:20:07 +0530

    Added slider

---
+++ client/src/components/slider/Slider.jsx
@@ -0,0 +1,71 @@
+import React, { useRef, useState } from "react";
+import KeyboardArrowLeftOutlinedIcon from "@mui/icons-material/KeyboardArrowLeftOutlined";
+import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+import "./Slider.css";
+import { useEffect } from "react";
+import { sliderItems } from "../../data";
+
+const Slider = () => {
+
+
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const ref = useRef(0);
+  const te = useRef("black");
+  const bg = useRef("white");
+
+  useEffect(() => {
+    ref.current.style.transform = `translateX(${-slideIndex * 100}vw)`;
+    console.log(ref.current.style);
+  }, [slideIndex]);
+
+
+  // setInterval(() => {
+  //   console.log("done");
+  //   console.log(te.current.style?.color)
+  //   if (te.current.style?.color===undefined) {}
+  //   else
+  //     te.current.style.color =
+  //       te.current.style.color === "red" ? "black" : "red";
+  // }, 500);
+
+
+  const handleClick = (direction) => {
+    console.log(direction);
+    direction === "left"
+      ? setSlideIndex((slideIndex) => (slideIndex === 0 ? sliderItems.length-1 : slideIndex - 1))
+      : setSlideIndex((slideIndex) => (slideIndex === sliderItems.length-1 ? 0 : slideIndex + 1));
+  };
+  return (
+    <div className="slider">
+      <div className="arrow left">
+        <KeyboardArrowLeftOutlinedIcon onClick={() => handleClick("left")} />
+      </div>
+      <div className="slide-wrapper" ref={ref}>
+
+        {
+          sliderItems.map(slide=>
+          <div className="slide" style={{backgroundColor:slide.bg}}>
+          <div className="imgContainer">
+            <img src={slide.img} />
+          </div>
+          <div className="infoContainer">
+            <h1 className="title" ref={te}>
+              {slide.title}
+            </h1>
+            <p className="desc">{slide.desc}</p>
+            <div className="button">
+              <button>SHOP NOW</button>
+            </div>
+          </div>
+        </div>)
+        }
+      </div>
+      <div className="arrow right">
+        <KeyboardArrowRightOutlinedIcon onClick={() => handleClick("right")} />
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Announcement from "../components/announcement/Announcement";
+import Categories from "../components/categories/Categories";
 import Navbar from "../components/navbar/Navbar";
+import Slider from "../components/slider/Slider";
 
 const Home = () => {
   return (
     <div className="container">
       <Announcement/>
         <Navbar/>
+        <Slider/>
+        <Categories/>
     </div>
   );
 };
