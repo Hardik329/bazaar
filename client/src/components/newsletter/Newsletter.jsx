f08491233fc3a7a533b68398e7cbef15abc38274
hardik <hardik32904@gmail.com>
2023-06-20 14:55:18 +0530

    Added Products and Newsletter component

---
+++ client/src/components/newsletter/Newsletter.jsx
@@ -0,0 +1,23 @@
+
+import SendIcon from '@mui/icons-material/Send';
+import './Newsletter.css'
+
+
+const Newsletter = () => {
+  return (
+    <div className="news-container">
+      <div className="news-title">Newsletter</div>
+      <div className="news-desc">
+        Get timely updates from your favorite products.
+      </div>
+      <div className="news-input-container">
+        <input placeholder="your email" className="news-input" />
+          <button className='news-button'>
+            <SendIcon />
+          </button>
+      </div>
+    </div>
+  );
+};
+
+export default Newsletter;
