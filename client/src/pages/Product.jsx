bf9f30a670742999aaf28dbb0c0dafb79e3998d0
hardik <hardik32904@gmail.com>
2023-06-27 22:33:37 +0530

    Added Product List

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,34 +1,44 @@
-
 import { useEffect, useState } from "react";
 import Footer from "../components/footer/Footer";
 import Newsletter from "../components/newsletter/Newsletter";
 import Navbar from "../components/navbar/Navbar";
 import Announcement from "../components/announcement/Announcement";
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import { useLocation } from "react-router-dom";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { useLocation, useParams } from "react-router-dom";
 
-import './Product.css'
-
+import "./Product.css";
+import { publicRequest } from "../useFetch";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
 
 const Product = () => {
-  const location = useLocation();
-  const id = location.pathname.split("/")[2];
-  const [product, setProduct] = useState({color:["blue","red","green"],size:["5",6],price:58});
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }, []);
+
+  const params = useParams();
+  const id = params.id;
+
+  const [product, setProduct] = useState({
+    color: ["blue", "red", "green"],
+    size: ["5", 6],
+    price: 58,
+  });
   const [quantity, setQuantity] = useState(1);
   const [color, setColor] = useState("");
   const [size, setSize] = useState("");
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   const getProduct = async () => {
-  //     try {
-  //       const res = await publicRequest.get("/products/find/" + id);
-  //       setProduct(res.data);
-  //     } catch {}
-  //   };
-  //   getProduct();
-  // }, [id]);
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const res = await publicRequest.get("/products/find/" + id);
+        setProduct(res.data);
+      } catch {}
+    };
+    getProduct();
+  }, [id]);
 
   const handleQuantity = (type) => {
     if (type === "dec") {
@@ -39,9 +49,7 @@ const Product = () => {
   };
 
   const handleClick = () => {
-    // dispatch(
-    //   addProduct({ ...product, quantity, color, size })
-    // );
+    dispatch(addToCart({ ...product, quantity, color, size }));
   };
   return (
     <div className="p-container">
@@ -59,26 +67,35 @@ const Product = () => {
             <div className="p-filter">
               <span className="p-filter-title">Color</span>
               {product.color?.map((c) => (
-                <div className="p-filter-color" style={{backgroundColor:c}} onClick={() => setColor(c)} />
+                <div
+                  className="p-filter-color"
+                  style={{ backgroundColor: c }}
+                  onClick={() => setColor(c)}
+                />
               ))}
             </div>
             <div className="p-filter">
               <span className="p-filter-title">Size</span>
-              <select className="p-filter-size" onChange={(e) => setSize(e.target.value)}>
+              <select
+                className="p-filter-size"
+                onChange={(e) => setSize(e.target.value)}
+              >
                 {product.size?.map((s) => (
                   <option key={s}>{s}</option>
                 ))}
               </select>
             </div>
           </div>
-          
+
           <div className="p-add-container">
             <div className="p-amount-container">
               <RemoveIcon onClick={() => handleQuantity("dec")} />
               <div className="p-amount">{quantity}</div>
               <AddIcon onClick={() => handleQuantity("inc")} />
             </div>
-            <button className="p-button" onClick={handleClick}>ADD TO CART</button>
+            <button className="p-button" onClick={handleClick}>
+              ADD TO CART
+            </button>
           </div>
         </div>
       </div>
