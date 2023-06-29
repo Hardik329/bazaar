86971e7bff35c24bfba735456ad2dee064a87d91
hardik <hardik32904@gmail.com>
2023-06-29 18:00:28 +0530

    Added wishlist animation

--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    name:{type:String, required: true},
-    lastname:{type:String},
+    name: { type: String, required: true },
+    lastname: { type: String },
     isAdmin: {
       type: Boolean,
       default: false,
