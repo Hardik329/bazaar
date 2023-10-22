ea305694557e571cc4e46ccf5b0b112f84151a07
hardik <hardik32904@gmail.com>
2023-10-22 15:09:43 +0530

    Debugging

--- server/index.js
+++ server/index.js
@@ -12,9 +12,9 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  console.log = () => {};
-}
+// if (process.env.NODE_ENV === "production") {
+//   console.log = () => {};
+// }
 
 dotenv.config();
 
