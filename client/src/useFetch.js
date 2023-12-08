c1ca07557df6ad14423f6ad633a28c257c3b0011
hardik <hardik32904@gmail.com>
2023-12-08 19:12:24 +0530

    Added redis cache

--- client/src/useFetch.js
+++ client/src/useFetch.js
@@ -6,9 +6,10 @@ const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
 
-const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://fair-cyan-gazelle-tie.cyclic.app/api";
+
 // const BASE_URL = "https://baazaar-backend.onrender.com/api";
-// const BASE_URL = "https://fair-cyan-gazelle-tie.cyclic.app/api";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
