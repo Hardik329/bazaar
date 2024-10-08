26e577823bc542f52cc5683c58321f35cbda7ad6
hardik <hardik32904@gmail.com>
2024-10-08 19:47:29 +0530

    Add server endpoint to .env

--- client/src/useFetch.js
+++ client/src/useFetch.js
@@ -1,15 +1,11 @@
 import axios from "axios";
+import { BASE_URL } from "./utils/constants";
 
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 // const TOKEN = store.getState()?.currentUser?.accessToken;
 
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
-
-// const BASE_URL = "http://localhost:5000/api";
-const BASE_URL = "https://fair-cyan-gazelle-tie.cyclic.app/api";
-
-// const BASE_URL = "https://baazaar-backend.onrender.com/api";
 
 
 export const userRequest = axios.create({
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,4 @@
 export const CDN_URL =
   "https://res.cloudinary.com/di5i4j5uv/image/upload/c_fill,f_auto,q_auto,w_auto:100/";
+
+  export const BASE_URL = process.env.REACT_APP_BASE_URL
